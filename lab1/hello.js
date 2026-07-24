fe1237d9e79f730aa21ce78810af03afef43f6a5
Anxhu <akvtechmoy@gmail.com>
2026-07-24 14:53:39 +0530

    initial commit with lab1 folder structure

---
+++ lab1/hello.js
@@ -0,0 +1,29 @@
+console.log("hello");
+
+// write a function to take two number and return
+// their sum
+function sum(a, b) {
+  return a + b;
+}
+const add = (a, b) => {
+  return a + b;
+};
+
+const add2 = (a, b) => a + b;
+
+console.log(sum(2, 3));
+console.log(add(2, 3));
+console.log(add2(2, 3));
+
+
+
+//react frontend
+// VEW.js
+// FLASK
+// Angular
+// css
+// (BOOTSTRAP)
+// Tailwind
+
+//Backend
+//Node.js, Springboot, Fast API, Django, PHP
