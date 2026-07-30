1a0fad0ec7960b3769061d60322fef0ee49425de
Akv Tech <Akvtechmoy@gmail.com>
2026-07-30 12:00:53 +0530

    lab1-done

--- lab1/dom5.js
+++ lab1/dom5.js
@@ -1,4 +1,4 @@
-import {EventEmitter} from './event-emitter.js';
+import { EventEmitter } from 'events';
 class DomClass extends EventEmitter {
     addEventListener(eventName, callback) {
         this.on(eventName, callback);
